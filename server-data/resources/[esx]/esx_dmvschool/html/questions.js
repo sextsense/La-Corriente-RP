de12f090f7a4e82ce95bd1298d156fa42663293b
Erikete93 <109676139+Erikete93@users.noreply.github.com>
2022-07-21 01:13:47 +0200

    Add files via upload

---
+++ server-data/resources/[esx]/esx_dmvschool/html/questions.js
@@ -0,0 +1,91 @@
+﻿var tableauQuestion = [
+	{
+		question: "Si vas a 80 km / h, y te aproximas a una zona residencial, debes:",
+		propositionA: "Haces un botellón con todos los residentes",
+		propositionB: "Mantienes tu velocidad, si no adelantas otros vehículos.",
+		propositionC: "Frenas",
+		propositionD: "Mantienes tu velocidad",
+		reponse: "C"
+	},
+
+	{
+		question: "Si está girando a la derecha en un semáforo, pero ve un cruce de peatones con un peaton, ¿qué hace?:",
+		propositionA: "Atropellas al peaton",
+		propositionB: "Chequeas que no haya vehiculos alrededor",
+		propositionC: "Esperas mientras el peaton cruza",
+		propositionD: "Disparas al peaton y sigues conduciendo",
+		reponse: "C"
+	},
+
+	{
+		question: "Sin prioridad de señal, cual es la velocidad máxima en la vía: __ km/h",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "El velocímetro entero que para algo lo he pagado",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Antes de cada cambio de carril debes:",
+		propositionA: "Mirar tus espejos retrovisores",
+		propositionB: "Chequear tus puntos ciegos",
+		propositionC: "Señalizar tu movimiento",
+		propositionD: "Todos los anteriores",
+		reponse: "D"
+	},
+
+	{
+		question: "Cual es la tasa de alcohol en sangre maxima:",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "Al borde del coma etílico",
+		reponse: "C"
+	},
+
+	{
+		question: "¿Cuándo puedes continuar conduciendo en un semáforo?",
+		propositionA: "Cuando esta verde",
+		propositionB: "Cuando no hay nadie en la interseccion",
+		propositionC: "Cuando no veas policías",
+		propositionD: "Cuando esta en verde o ambar",
+		reponse: "D"
+	},
+
+	{
+		question: "Un peatón tiene una señal de no cruzar, ¿qué haces?",
+		propositionA: "Atropellarlo",
+		propositionB: "Observas antes de continuar",
+		propositionC: "Lo sigues hasta su casa para cruzarle la cara",
+		propositionD: "Continuas por que la luz del semaforo es verde",
+		reponse: "D"
+	},
+
+	{
+		question: "Lo que está permitido al adelantar a otro vehículo.",
+		propositionA: "Lo sigues de cerca para pasarlo más rápido",
+		propositionB: "Se pasa sin salir de la calzada.",
+		propositionC: "Conduce por el lado opuesto de la carretera para pasar.",
+		propositionD: "Se supera el límite de velocidad para pasarlos.",
+		reponse: "C"
+	},
+
+	{
+		question: "Usted está conduciendo en una carretera que indica una velocidad máxima de 120 km / h. Pero la mayoría de los conductores conducen a 125 km / h, por lo que no debe conducir más rápido que:",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "Hasta el infinito y más allá",
+		reponse: "A"
+	},
+
+	{
+		question: "Cuando es superado por otro vehículo, es importante NO:",
+		propositionA: "Frenar",
+		propositionB: "Pegarle al conductor en el siguiente semáforo",
+		propositionC: "Echarle un pique",
+		propositionD: "Acelerar",
+		reponse: "D"
+	},
+]
