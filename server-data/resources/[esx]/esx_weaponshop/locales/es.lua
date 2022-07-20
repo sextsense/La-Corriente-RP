72338507d09b35375d8b78be64c6de5edb96a7be
Erikete93 <109676139+Erikete93@users.noreply.github.com>
2022-07-21 01:14:38 +0200

    Add files via upload

---
+++ server-data/resources/[esx]/esx_weaponshop/locales/es.lua
@@ -0,0 +1,13 @@
+Locales ['es'] = {
+  ['buy_license'] = 'comprar licencia de armas',
+  ['yes'] = '%s',
+  ['no'] = 'no',
+  ['weapon_bought'] = 'comprada por %s $',
+  ['not_enough_black'] = 'usted no tiene ~r~suficiente~s~ dinero negro',
+  ['not_enough'] = 'usted no tiene ~r~suficiente~s~ dinero',
+  ['already_owned'] = 'ya posees este arma!',
+  ['shop_menu_title'] = 'tienda',
+  ['shop_menu_prompt'] = 'presione ~INPUT_CONTEXT~ para acceder a la tienda.',
+  ['shop_menu_item'] = '%s $',
+  ['map_blip'] = 'arsenal'
+}
