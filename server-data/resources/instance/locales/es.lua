24915f1012c6b8192187c3e1a63c546153d4ef54
sextsense <109617305+sextsense@users.noreply.github.com>
2022-07-20 22:23:51 +0200

    Add files via upload

---
+++ server-data/resources/instance/locales/es.lua
@@ -0,0 +1,8 @@
+Locales['es'] = {
+  ['left_instance'] = 'has abandonado la instancia',
+  ['invite_expired'] = 'invitación expirada',
+  ['press_to_enter'] = 'presiona ~INPUT_CONTEXT~ para entrar a la instancia',
+  ['entered_instance'] = 'has entrado a la instancia',
+  ['entered_into'] = '%s ha entrado en la instancia',
+  ['left_out'] = '%s ha abandonado la instancia',
+}
