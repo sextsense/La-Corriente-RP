05d35652acc8e9ae3366c0dcf01d25cfcc473211
sextsense <109617305+sextsense@users.noreply.github.com>
2022-07-23 00:31:47 +0200

    Add files via upload

---
+++ server-data/resources/[esx]/esx_garage/locales/es.lua
@@ -0,0 +1,5 @@
+Locales['es'] = {
+	['veh_released'] = 'el vehiculo ha sido ~g~publicado',
+	['veh_stored'] = 'el vehiculo ha sido ~g~almacenado',
+	['veh_health'] = 'necesitas reparar el vehiculo antes de almacenarlo.',
+}
